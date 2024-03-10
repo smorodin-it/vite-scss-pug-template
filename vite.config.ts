31f71b3ba13fa0ad87ba61a5456bf6025c208b88
smorodin-it <smorodin.it@gmail.com>
2024-03-11 01:08:51 +0300

    configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite';
-import pugPlugin from 'vite-plugin-pug';
-import * as path from 'path';
+import { defineConfig } from "vite";
+import pugPlugin from "vite-plugin-pug";
+import * as path from "path";
 
 export default defineConfig({
-  root: './src',
-  plugins: [pugPlugin()],
-  publicDir: '../public',
-  build: {
-    outDir: '../dist',
-    rollupOptions: {
-      input: {
-        app: './src/pages/index.html',
-      },
+    root: "./src",
+    plugins: [pugPlugin()],
+    publicDir: "../public",
+    build: {
+        outDir: "../dist",
+        rollupOptions: {
+            input: {
+                app: "./src/pages/index.html",
+            },
+        },
     },
-  },
-  resolve: {
-    alias: { '/src': path.resolve(process.cwd(), 'src') },
-  },
-  server: {
-    open: './pages/index.html',
-  },
+    resolve: {
+        alias: { "/src": path.resolve(process.cwd(), "src") },
+    },
+    server: {
+        open: "./pages/index.html",
+    },
 });
