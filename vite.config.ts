293bd462dd9c557641b709feccb0ec069066a8b5
smorodin-it <smorodin.it@gmail.com>
2024-03-11 22:46:54 +0300

    format html with prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,32 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import pugPlugin from "vite-plugin-pug";
 import * as path from "path";
+import * as prettier from "prettier";
+
+const formatHtmlPlugin = (): PluginOption => ({
+    name: "beautify-html",
+    transformIndexHtml: {
+        order: "post",
+        handler: async (html) =>
+            html.replace(
+                html,
+                await prettier.format(html, {
+                    parser: "html",
+                    htmlWhitespaceSensitivity: "ignore",
+                    tabWidth: 4,
+                    printWidth: 100,
+                }),
+            ),
+    },
+});
 
 export default defineConfig({
     root: "./src",
-    plugins: [pugPlugin()],
+    plugins: [pugPlugin(), formatHtmlPlugin()],
     publicDir: "../public",
     build: {
         outDir: "../dist",
+        emptyOutDir: true,
         rollupOptions: {
             input: {
                 app: "./src/pages/index.html",
